f3079d35280515c8d884fd6300ba4d2b8cf54892
KhasanTursunov <khasantursunovrustamovich@gmail.com>
2024-09-28 14:15:13 +0500

    changed for mobile

---
+++ main.js
@@ -0,0 +1,25 @@
+// 1-masala
+// function multiples(m, n) {
+//   let result = [];
+//   for (let i = 1; i <= m; i++) {
+//     result.push(n * i);
+//   }
+//   return result;
+// }
+
+// console.log(multiples(3, 5.0));
+
+
+// 2-masala
+
+// function Solution(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 !== 0) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// let arr = [4, -8, 98, -12, -7, 90, 100];
+// console.log(Solution(arr));
